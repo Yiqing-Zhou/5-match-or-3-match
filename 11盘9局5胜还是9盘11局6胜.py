0ef5418ae3064e6f24d2e1017602e100825e5285
Yiqing Zhou <muzhang79@163.com>
2019-07-06 00:16:49 +0800

    dff

--- 11盘9局5胜还是9盘11局6胜.py
+++ 11盘9局5胜还是9盘11局6胜.py
@@ -1,30 +1,74 @@
-# -*- coding: utf-8 -*-
-
 from matplotlib import pyplot as plt
 import numpy as np
 from scipy.special import comb
 
-def calculate_pro(n,p):
-    #p为单局胜率，n为比赛总局数,输出总胜率
-    assert n%2!=0#n如果是偶数就报错
-    m=(n+1)//2
-    result=0
-    for i in range(m,n+1):#胜利次数
-        result+=comb(n,i)*np.power(p,i)*np.power((1-p),n-i)
+
+def f(p, n):
+    assert n % 2 != 0
+    m = (n + 1) // 2
+    result = 0
+    for i in range(m, n + 1):
+        result += comb(n, i) * np.power(p, i) * np.power((1 - p), n - i)
     return result
-def calculate_success(a,b):#a 盘 b局(b+1)/2胜 的胜算
-    p_success=[]
-    p_range=list(np.linspace(0.5,1,20))#这里可以调整p的取值范围(0.5,1)，以及点数(20)
+    
+
+def r(p):
+    return p * (1 - p)
+
+
+def df(p, n):
+    assert n % 2 != 0
+    m = (n - 1) // 2
+    return n * comb(2 * m, m) * np.power(r(p), m) 
+
+
+def draw(func, *args):
+    t_list = []
+    p_range = list(np.linspace(0.5, 1, 20))
     for p in p_range:
-        p_success.append(calculate_pro(a,calculate_pro(b,p)))
-    return plt.plot(p_range,p_success,label='times:%d'%a)#代表盘数
-calculate_success(11,9)#11盘9局5胜
-calculate_success(9,11)#9盘11局6胜
-calculate_success(3,33)#3盘33局17胜
-calculate_success(33,3)#33盘3局2胜
-calculate_success(99,1)#99盘1局1胜
-calculate_success(1,99)#1盘99局50胜
-plt.xlabel('p for every match')
+        t_list.append(func(p, *args))
+    return plt.plot(p_range, t_list, label=str(args))
+
+
+def ff(p, a, b):
+    return f(f(p, b), a)
+
+
+def dff(p, a, b):
+    q_b = f(p, b)
+    return df(q_b, a) * df(p, b)
+
+
+def srl(p, a):
+    q_a = f(p, a)
+    return np.power(r(q_a) / r(p), 2 / (a - 1))
+
+
+def dsrl(p, a):
+    d_q = df(p, a)
+    d_q_plus = df(p, a + 2)
+    d_q_minus = df(p, a - 2)
+    return d_q_plus * d_q_minus / d_q / d_q
+
+
+# draw(ff, 11, 9)
+# draw(ff, 9, 11)
+
+
+# draw(dff, 33, 3)
+# draw(dff, 3, 33)
+
+
+# for i in range(3, 33, 2):
+#     draw(srl, i)
+
+
+for i in range(3, 33, 2):
+    draw(dsrl, i)
+
+
+plt.xlabel('t')
 plt.ylabel('p')
 plt.legend()
+plt.show()
 
